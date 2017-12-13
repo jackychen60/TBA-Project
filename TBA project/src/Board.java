05c974285323aafdb3e8c035db337cb1fa4cc314
BT_1N3_07 <BT_1N3_07@BT_1N3_07.bktech.local>
2017-12-13 09:25:37 -0500

    zf

---
+++ TBA project/src/Board.java
@@ -0,0 +1,23 @@
+
+public class Board {
+private Room[][] rooms;
+public Board(Room[][]rooms)
+{
+	this.rooms=rooms;
+}
+public Board(int size)
+{
+	this.rooms= new Room[size][size];
+}
+public void printBoard()
+{
+	for(Room[]i:rooms)
+	{
+		for(Room x:i) 
+		{
+			x.print();
+		}
+	}
+	System.out.println();
+}
+}
--- TBA project/src/Room.java
+++ TBA project/src/Room.java
@@ -21,4 +21,8 @@ public class Room {
 		occupant = null;
 	}
 	
+	public  print()
+	{
+		
+	}
 }
