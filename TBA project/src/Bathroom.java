855bb71bd928cbe6cd634fb82a7345a116ce447b
BT_1N3_07 <BT_1N3_07@BT_1N3_07.bktech.local>
2018-01-03 09:24:47 -0500

    fgafdf

--- TBA project/src/Bathroom.java
+++ TBA project/src/Bathroom.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Bathroom extends Room{
 
 	public Bathroom(int x, int y) {
@@ -7,11 +9,26 @@ public class Bathroom extends Room{
 	@Override
 	public void enterRoom(Person x) {
 		// TODO Auto-generated method stub
+		
 		setExplored(true);
 		occupant = x;
 		x.setxLoc(this.xLoc);
 		x.setyLoc(this.yLoc);
-		System.out.println("This is the mens room");
+		System.out.println("This is the mens room.");
+		System.out.println("What would you like to do?");
+		System.out.println("1.Search the bathroom.");
+		System.out.println("2.Leave.");
+		if(choose(a=1))
+		{
+			System.out.println("You found a clue.");
+		}
+		
+		}
+	
+	public static int choose(int a)
+	{
+		Scanner in = new Scanner(System.in);
+		return a = in.nextInt();
 		
 	}
 }
